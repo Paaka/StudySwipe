3513559d4f5f2af1a0aeb00b32f006443006b8d1
Mateusz Hartabus <32105930+Paaka@users.noreply.github.com>
2023-06-10 16:25:28 +0200

    feat(set): created two lists

--- study-swipe/src/pages/set/Set.tsx
+++ study-swipe/src/pages/set/Set.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { IFlashcard } from '../../models/flashcard.interface';
 import './Set.scss';
 
-function Set() {
+const Set = () => {
   const [flashCards, setFlashCards] = useState<IFlashcard[]>([
     { id: '0', keyword: 'Subaru 22B', definition: 'Best car ever produced' },
   ]);
@@ -14,12 +14,22 @@ function Set() {
       <Link to="/set/3/2">
         <Button>Go to specyfic button</Button>
       </Link>
-      <h1>Keyword</h1>
-      {flashCards.map((flashCard) => (
-        <Card>{flashCard.keyword}</Card>
-      ))}
+      <div className="wrapper">
+        <div className="wrapper__list">
+          <h1>Keyword</h1>
+          {flashCards.map((flashCard) => (
+            <Card>{flashCard.keyword}</Card>
+          ))}
+        </div>
+        <div className="wrapper__list">
+          <h1>Definition</h1>
+          {flashCards.map((flashCard) => (
+            <Card>{flashCard.definition}</Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Set;
