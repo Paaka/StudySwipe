535feaf21b196e85d192f057ae128de2087ca62c
Mateusz Hartabus <32105930+Paaka@users.noreply.github.com>
2023-07-14 23:55:48 +0200

    refactor(study): small improvments

--- study-swipe/src/pages/study/Study.tsx
+++ study-swipe/src/pages/study/Study.tsx
@@ -3,7 +3,16 @@ import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { ApplicationState, ISetsState } from '../../models/state.interfaces';
 import { SetCard } from '../../models/set-card.interface';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Icon,
+  IconButton,
+  Typography,
+} from '@mui/material';
+import { VolumeUp } from '@mui/icons-material';
 
 const Study = () => {
   const dispatch = useDispatch();
@@ -25,15 +34,23 @@ const Study = () => {
   const test2 = currentFlashcard[currentKeyword];
   let speach = new SpeechSynthesisUtterance(test2);
 
-  const test = () => {
+  const onSpeakHandler = () => {
     window.speechSynthesis.speak(speach);
   };
 
   return (
-    <div data-cy="study" className="study">
-      <p>{test2}</p>
-      <button onClick={test}>Click me</button>
-    </div>
+    <>
+      <Link to="/set/0">Go back</Link>
+      <div data-cy="study" className="study">
+        <Card>
+          <Typography variant="h6">{currentKeyword}</Typography>
+          <CardContent>{test2}</CardContent>
+        </Card>
+        <IconButton onClick={onSpeakHandler}>
+          <VolumeUp></VolumeUp>
+        </IconButton>
+      </div>
+    </>
   );
 };
 
